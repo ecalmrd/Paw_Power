2d02999d4faf674a12cfe9e8024502492c381fcd
AForkintheCode <96763461+AForkintheCode@users.noreply.github.com>
2023-01-30 23:35:55 -0800

    merged/renamed files, fixed conflicts

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// Pseudo Code for Home Page
+// Create add event listeners for adopting cat or dog
+// Choosing a cat will display all cats, choosing a dog will display all dogs
+// optional: browse both cats and dogs
+// create search bar to add location
+
+//Pseudo Code for browsing pets apage
+// This page includes a location bar at top and how many miles within current location
+// Filter options to sort distance from nearest to farthest
+// Additional filters to include age, breed, color, size, gender, coat, etc
+// When browsing, each profile will display a picture of the pet and include basic information such as age, name, breed, and distance.
+// Each profile will have option to favorite a pet
+
+
+
+
+{
+
+}
