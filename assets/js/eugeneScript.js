ab660f2db33c043ed1d54ad6efd6fe4c5050ab1b
Gene Calm <ecalmrd@gmail.com>
2023-02-02 18:58:55 -0800

    small update

--- assets/js/eugeneScript.js
+++ assets/js/eugeneScript.js
@@ -9,9 +9,6 @@ var petfinderAnimalsAPI = "https://api.petfinder.com/v2/animals";
 var petfinderBreedsAPI = "https://api.petfinder.com/v2/types/{type}/breeds"
 var petfinderOrgAPI = "https://api.petfinder.com/v2/organizations";
 
-//Pet Profile Page variables
-
-
 
 fetch('https://api.petfinder.com/v2/oauth2/token', {
     method: 'POST',
@@ -26,7 +23,8 @@ fetch('https://api.petfinder.com/v2/oauth2/token', {
     token = response.access_token
     console.log(token)
     //change query parameters here
-    fetch('https://api.petfinder.com/v2/animals?type=dog&page=2', {
+
+fetch('https://api.petfinder.com/v2/animals?type=dog&page=2', {
         method: 'GET',
         headers: {
             'Authorization': `Bearer ${token}`
@@ -38,10 +36,63 @@ fetch('https://api.petfinder.com/v2/oauth2/token', {
     })
 })
 
-//this code will take animal characteristics from petfinder API 
-//and display in profile page
 
-function perCharacteristics() {
+//code to show current location
+//https://developers.google.com/maps/documentation/javascript/geolocation#maps_map_geolocation-javascript
+let map, infoWindow;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 6,
+  });
+  infoWindow = new google.maps.InfoWindow();
 
+  const locationButton = document.createElement("button");
 
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
 }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+
+window.initMap = initMap;
+
+//end of current location code
+
+
+
+//Pet Profile Page variables
+
