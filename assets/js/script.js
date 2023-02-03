f4362480d65e863ff196f6ed82265777c29b55b1
AForkintheCode <96763461+AForkintheCode@users.noreply.github.com>
2023-02-02 18:59:14 -0800

    token

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,229 @@
 // Pseudo Code for Home Page
+$(document).ready(function (){
 
-// curl -d "grant_type=client_credentials&client_id=EmpbeFp7f6MKXl7XkxoSG64fRk4kLmwsy3mkt1KGUpsZunCWBp&client_secret=fb4tKOw40Veks4aKEFdaZ5yQPl5SgwfxzsFDemc2" https://api.petfinder.com/v2/oauth2/token
+var n1;
+var n2;
+var n3;
+var n4;
+var d1;
+var d2;
+var d3;
+var d4;
+var p1;
+var p2;
+var p3;
+var p4;
 
-// curl -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJFbXBiZUZwN2Y2TUtYbDdYa3hvU0c2NGZSazRrTG13c3kzbWt0MUtHVXBzWnVuQ1dCcCIsImp0aSI6IjNjZGNkMGNiZTM5NDE3OWQzMTlhMDRiNDNhMjNiZGY3MDcyNDgxOWEzMWJhMDY1OTg3ZjlmYjY2MjM4ZTU2NjM2MGNkMDkwOTc0YzI0YTFmIiwiaWF0IjoxNjc1MzA2MjE5LCJuYmYiOjE2NzUzMDYyMTksImV4cCI6MTY3NTMwOTgxOSwic3ViIjoiIiwic2NvcGVzIjpbXX0.MxYK9LEGaZKUbBafZwajGVPzB1WEoUBInKbzzyogRDDrhJWHnE9SvmWxfQ_dmhgP4wXko4ibRoxD0JC5kJck8td7zShoEtZtwaT2PZu1PgRdNgn_9YFJdGwVUnfz6m_wKTNfep9eFWNz_C5pFS2hLAGQYUMCOvVx01CHjXofVrqdR4CeYgZcQag3ftspmlpNSQX8VhKyOokVg1bxF1UIKRyJMTDrtp9nmwpaFwlAa_s_FzNK6TM8MKAkw3C66gTEcl7Cuj15WIyt0NpJVcwS_A7-jOExoe8zRm8UZK7VvmYHTdNi26WI0SuV49f4P3LbAKOClLSyJs_taMioeQ582w" GET https://api.petfinder.com/v2/types/cat
+document.getElementById("results").style.display = 'none'
 
+var ID = 'EmpbeFp7f6MKXl7XkxoSG64fRk4kLmwsy3mkt1KGUpsZunCWBp'
+var secret = 'fb4tKOw40Veks4aKEFdaZ5yQPl5SgwfxzsFDemc2'
+var token;
 
+fetch('https://api.petfinder.com/v2/oauth2/token', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: `grant_type=client_credentials&client_id=${ID}&client_secret=${secret}`
+}).then((response) => {
+    return response.json()
+}).then((response) => {
+    console.log(response)
+    token = response.access_token
+    console.log(token)
+    //change query parameters here
+    fetch('https://api.petfinder.com/v2/animals?type=dog&page=2', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    }).then((response) => {
+        return response.json()        
+    }).then((response) => {
+        console.log(response.animals)
+    })
+})
 
-// var ID= 'EmpbeFp7f6MKXl7XkxoSG64fRk4kLmwsy3mkt1KGUpsZunCWBp'
-// var secret= 'fb4tKOw40Veks4aKEFdaZ5yQPl5SgwfxzsFDemc2'
+function petDeets(){
+    n1 = response.animals[0].name;
+    d1 = response.animals[0].description;
+    p1 = response.animals[0].photos[0].medium    
+    n2 = response.animals[1].name;
+    d2 = response.animals[1].description;
+    n3 = response.animals[2].name;
+    d3 = response.animals[2].description;
+    n4 = response.animals[3].name;
+    d4 = response.animals[3].description;
+    document.getElementById("name-1").innerHTML=n1;
+    document.getElementById("desc-1").innerHTML=d1;
+    document.getElementById("name-2").innerHTML=n2;
+    document.getElementById("desc-2").innerHTML=d2;
+    document.getElementById("name-3").innerHTML=n3;
+    document.getElementById("desc-3").innerHTML=d3;
+    // document.getElementById("name-4").innerHTML=n4;
+    // document.getElementById("desc-4").innerHTML=d4;    
+}
 
-// fetch('https://api.petfinder.com/v2/oauth2/token', {
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/x-www-form-urlencoded'
-//     },
-//     body: `grant_type=client_credentials&client_id=${ID}&client_secret=${secret}`    
-// });
-
-// fetch("https://api.petfinder.com/v2/{CATEGORY}/{ACTION}?{parameter_1}={value_1}&{parameter_2}={value_2}", {
-//   headers: {
-//     Authorization: "Bearer {YOUR_ACCESS_TOKEN}"
-//   }
-// })
 
 
 // Create add event listeners for adopting cat or dog
-$('#catbutton').on("click", function(e) {
+$('#catbutton').on("click", function (e) {
     console.log('I choose cats!')
-    // chooseCat();
+    document.getElementById("results").style.display = 'block';
+    chooseCat();
+    
 });
 // Choosing a cat will display all cats
-function chooseCat(){
-    window.location.href = "./index.html"
+function chooseCat() {
+    fetch('https://api.petfinder.com/v2/oauth2/token', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: `grant_type=client_credentials&client_id=${ID}&client_secret=${secret}`
+}).then((response) => {
+    return response.json()
+}).then((response) => {
+    console.log(response)
+    token = response.access_token    
+    //change query parameters here
+    fetch('https://api.petfinder.com/v2/animals?type=cat&page=4', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    }).then((response) => {
+        return response.json()        
+    }).then((response) => {
+        console.log(response.animals) 
+        n1 = response.animals[0].name;
+        d1 = response.animals[0].description;           
+        n2 = response.animals[1].name;
+        d2 = response.animals[1].description;
+        n3 = response.animals[2].name;
+        d3 = response.animals[2].description;
+        n4 = response.animals[3].name;
+        d4 = response.animals[3].description;
+        document.getElementById("name-1").innerHTML=n1;
+        document.getElementById("desc-1").innerHTML=d1;
+        document.getElementById("name-2").innerHTML=n2;
+        document.getElementById("desc-2").innerHTML=d2;
+        document.getElementById("name-3").innerHTML=n3;
+        document.getElementById("desc-3").innerHTML=d3;
+        // document.getElementById("name-4").innerHTML=n4;
+        // document.getElementById("desc-4").innerHTML=d4;  
+            
+    })    
+})
 }
 
+
 // Choosing a dog will display all dogs
-$('#dogbutton').on("click", function(e) { 
+$('#dogbutton').on("click", function (e) {
     console.log('I choose dogs!')
-    // chooseDog();
+    document.getElementById("results").style.display = 'block';
+    chooseDog();
+    
 });
 
-function chooseDog(){ 
-    window.location.href = "./index.html"
+function chooseDog() {
+    fetch('https://api.petfinder.com/v2/oauth2/token', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: `grant_type=client_credentials&client_id=${ID}&client_secret=${secret}`
+}).then((response) => {
+    return response.json()
+}).then((response) => {
+    console.log(response)
+    token = response.access_token    
+    //change query parameters here
+    fetch('https://api.petfinder.com/v2/animals?type=dog&page=4', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    }).then((response) => {
+        return response.json()        
+    }).then((response) => {
+        console.log(response.animals)
+        n1 = response.animals[0].name;
+        d1 = response.animals[0].description;
+        n2 = response.animals[1].name;
+        d2 = response.animals[1].description;
+        n3 = response.animals[2].name;
+        d3 = response.animals[2].description;
+        n4 = response.animals[3].name;
+        d4 = response.animals[3].description;
+        document.getElementById("name-1").innerHTML=n1;
+        document.getElementById("desc-1").innerHTML=d1;
+        document.getElementById("name-2").innerHTML=n2;
+        document.getElementById("desc-2").innerHTML=d2;
+        document.getElementById("name-3").innerHTML=n3;
+        document.getElementById("desc-3").innerHTML=d3;
+        // document.getElementById("name-4").innerHTML=n4;
+        // document.getElementById("desc-4").innerHTML=d4;        
+    })
+})   
 }
 
 // optional: browse both cats and dogs
-$('#petbutton').on("click", function(e) { 
+$('#petbutton').on("click", function (e) {
     console.log('Surprise me!')
-    // choosepet();
+    document.getElementById("results").style.display = 'block';
+    choosePet();
+    
 });
 
-function choosePet(){ 
-    window.location.href = "./index.html"
-}
+function choosePet() {
+    fetch('https://api.petfinder.com/v2/oauth2/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: `grant_type=client_credentials&client_id=${ID}&client_secret=${secret}`
+    }).then((response) => {
+        return response.json()
+    }).then((response) => {
+        console.log(response)
+        token = response.access_token        
+        //change query parameters here
+        fetch('https://api.petfinder.com/v2/animals?type=dog&page=2', {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).then((response) => {
+            return response.json()        
+        }).then((response) => {
+            console.log(response.animals)
+            n1 = response.animals[0].name;
+            d1 = response.animals[0].description;
+            n2 = response.animals[1].name;
+            d2 = response.animals[1].description;
+            n3 = response.animals[2].name;
+            d3 = response.animals[2].description;
+            n4 = response.animals[3].name;
+            d4 = response.animals[3].description;
+            document.getElementById("name-1").innerHTML=n1;
+            document.getElementById("desc-1").innerHTML=d1;
+            document.getElementById("img-1").innerHTML=p1
+            document.getElementById("name-2").innerHTML=n2;
+            document.getElementById("desc-2").innerHTML=d2;
+            document.getElementById("name-3").innerHTML=n3;
+            document.getElementById("desc-3").innerHTML=d3;
+            // document.getElementById("name-4").innerHTML=n4;
+            // document.getElementById("desc-4").innerHTML=d4;        
+        })
+    })   
+    }
+
+
 
 // create search bar to add location
+var location = document.getElementById('search').value
 
 //Pseudo Code for browsing pets apage
 // This page includes a location bar at top and how many miles within current location
@@ -69,5 +238,5 @@ function choosePet(){
 
 
 
-
+})
 
