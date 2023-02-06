6fe3355cf9c1c57bccd4ecfc9f7212011d7a3f12
Ryan Lierman <rylierman@gmail.com>
2023-02-06 13:34:27 -0800

    Basic tweeks and fixes minimal changes

--- assets/js/script-r.js
+++ assets/js/script-r.js
@@ -1,28 +1,4 @@
-//TOKEN CODE? CODE SOURCE = Sinclair on Slack
-fetch('https://api.petfinder.com/v2/oauth2/token', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: `grant_type=client_credentials&client_id=${ID}&client_secret=${secret}`
-}).then((response) => {
-    return response.json()
-}).then((response) => {
-    console.log(response)
-    token = response.access_token
-    console.log(token)
-    //change query parameters here
-    fetch('https://api.petfinder.com/v2/animals?type=dog&page=2', {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${token}`
-        }
-    }).then((response) => {
-        return response.json()        
-    }).then((response) => {
-        console.log(response.animals)
-    })
-})
+
 //END OF TOKEN CODE
 //START FOR THE IMAGE CAROUSEL CODE
 let slideIndex = 1;
