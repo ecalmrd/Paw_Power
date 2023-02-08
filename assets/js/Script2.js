c63d84cfccd2cc63e05cbbbb16140a1e5fa50a38
Ryan Lierman <rylierman@gmail.com>
2023-02-08 12:07:47 -0800

    WORKING WORKING WORKING WORKING THERE ARE 9 CARDS AND THEY ARE ?Working?

--- assets/js/Script2.js
+++ assets/js/Script2.js
@@ -7,28 +7,69 @@ var secret = 'v4wMb5hrxiYYsippsyz7nIL2UqmVsZfDSXpkdiX3'
 var token;
 
 // variables to DOM maniplate and add pet details to HTML
-var story = $('#story');
+//  0  //
 var petPics = $('#petPics');
 var petNameEl = $('#petname');
-var typeEl = $('#type');
 var breedEl = $('#breed');
 var ageEl = $('#age');
 var genderEl = $('#gender');
-var colorEl = $('#color');
-var locationEl = $('#location');
-
-
-
-
+//  0  //
+//  1  //
 var petPics1 = $('#petPics1');
 var petNameEl1 = $('#petname1');
 var breedEl1 = $('#breed1');
 var ageEl1 = $('#age1');
 var genderEl1 = $('#gender1');
-
-
-
-
+//  1  //
+//  2  //
+var petPics2 = $('#petPics2');
+var petNameEl2 = $('#petname2');
+var breedEl2 = $('#breed2');
+var ageEl2 = $('#age2');
+var genderEl2 = $('#gender2');
+//  2  //
+//  3  //
+var petPics3 = $('#petPics3');
+var petNameEl3 = $('#petname3');
+var breedEl3 = $('#breed3');
+var ageEl3 = $('#age3');
+var genderEl3 = $('#gender3');
+//  3  //
+//  4  //
+var petPics4 = $('#petPics4');
+var petNameEl4 = $('#petname4');
+var breedEl4 = $('#breed4');
+var ageEl4 = $('#age4');
+var genderEl4 = $('#gender4');
+//  4  //
+//  5  //
+var petPics5 = $('#petPics5');
+var petNameEl5 = $('#petname5');
+var breedEl5 = $('#breed5');
+var ageEl5 = $('#age5');
+var genderEl5 = $('#gender5');
+//  5  //
+//  6  //
+var petPics6 = $('#petPics6');
+var petNameEl6 = $('#petname6');
+var breedEl6 = $('#breed6');
+var ageEl6 = $('#age6');
+var genderEl6 = $('#gender6');
+//  6  //
+//  7  //
+var petPics7 = $('#petPics7');
+var petNameEl7 = $('#petname7');
+var breedEl7 = $('#breed7');
+var ageEl7 = $('#age7');
+var genderEl7 = $('#gender7');
+//  7  //
+//  8  //
+var petPics8 = $('#petPics8');
+var petNameEl8 = $('#petname8');
+var breedEl8 = $('#breed8');
+var ageEl8 = $('#age8');
+var genderEl8 = $('#gender8');
+//  8  //
 
 
 
@@ -82,31 +123,53 @@ fetchData = () => {
                     animalPhotosArray = data.animals[0].photos // this acquires obtains the array from photos
 
                     //this displays the pet details on html page, replacing the placeholder text
-                    typeEl.text(data.animals[0].species)
+
+
+                    //  0  //
                     breedEl.text(data.animals[0].breeds.primary)
                     ageEl.text(data.animals[0].age)
                     genderEl.text(data.animals[0].gender)
-                    colorEl.text(data.animals[0].colors.primary)
-                    locationEl.text(data.animals[0].distance)
                     petNameEl.text(data.animals[0].name)
-                    story.text(data.animals[0].description)
-
-
-
-
+                    //  1  //
                     breedEl1.text(data.animals[1].breeds.primary)
                     ageEl1.text(data.animals[1].age)
                     genderEl1.text(data.animals[1].gender)
                     petNameEl1.text(data.animals[1].name)
-
-
-
-
-
-
-
-
-
+                    //  2  //
+                    breedEl2.text(data.animals[2].breeds.primary)
+                    ageEl2.text(data.animals[2].age)
+                    genderEl2.text(data.animals[2].gender)
+                    petNameEl2.text(data.animals[2].name)
+                    //  3  //
+                    breedEl3.text(data.animals[3].breeds.primary)
+                    ageEl3.text(data.animals[3].age)
+                    genderEl3.text(data.animals[3].gender)
+                    petNameEl3.text(data.animals[3].name)
+                    //  4  //
+                    breedEl4.text(data.animals[4].breeds.primary)
+                    ageEl4.text(data.animals[4].age)
+                    genderEl4.text(data.animals[4].gender)
+                    petNameEl4.text(data.animals[4].name)
+                    //  5  //
+                    breedEl5.text(data.animals[5].breeds.primary)
+                    ageEl5.text(data.animals[5].age)
+                    genderEl5.text(data.animals[5].gender)
+                    petNameEl5.text(data.animals[5].name)
+                    //  6  //
+                    breedEl6.text(data.animals[6].breeds.primary)
+                    ageEl6.text(data.animals[6].age)
+                    genderEl6.text(data.animals[6].gender)
+                    petNameEl6.text(data.animals[6].name)
+                    //  7  //
+                    breedEl7.text(data.animals[7].breeds.primary)
+                    ageEl7.text(data.animals[7].age)
+                    genderEl7.text(data.animals[7].gender)
+                    petNameEl7.text(data.animals[7].name)
+                    //  8  //
+                    breedEl8.text(data.animals[8].breeds.primary)
+                    ageEl8.text(data.animals[8].age)
+                    genderEl8.text(data.animals[8].gender)
+                    petNameEl8.text(data.animals[8].name)
 
 
 
@@ -147,6 +210,9 @@ fetchData();
 function replacePlaceholder(data) {
     if (data.animals[0].photos[0].full) {
         petPics.attr('src', data.animals[0].photos[0].full);
+        console.log(data.animals[0].photos[0].full)
+    }
+    if (data.animals[1].photos[0].full) {
         petPics1.attr('src', data.animals[1].photos[0].full);
         console.log(data.animals[0].photos[0].full)
     }
